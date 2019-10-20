e84fa5f97fc0d6657e258b6b649363c84c53aeee
mohsen khodadadzadeh <black.roze2020@gmail.com>
2019-10-20 16:25:08 +0330

    edit xInset

--- UPCarouselFlowLayout/UPCarouselFlowLayout.swift
+++ UPCarouselFlowLayout/UPCarouselFlowLayout.swift
@@ -59,7 +59,8 @@ open class UPCarouselFlowLayout: UICollectionViewFlowLayout {
         let isHorizontal = (self.scrollDirection == .horizontal)
         
         let yInset = (collectionSize.height - self.itemSize.height) / 2
-        let xInset = ((collectionSize.width - self.itemSize.width) / 2) - 75
+        var xInset = ((collectionSize.width - self.itemSize.width) / 2) - 75
+        xInset = xInset < 15 ? 15 : xInset
         self.sectionInset = UIEdgeInsets.init(top: yInset, left: xInset, bottom: yInset, right: xInset)
         
         let side = isHorizontal ? self.itemSize.width : self.itemSize.height
